11366236db53bb87d9206bee793d4febc4847703
evaniewares <chisopsyelera@gmail.com>
2023-11-30 19:57:41 +0000

    Added readme file and implemented a function that create a new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the root node of the tree
+ * @value: Value to put in the new node
+ * 
+ * Return: New node
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+
+	return (new_node);
+}
