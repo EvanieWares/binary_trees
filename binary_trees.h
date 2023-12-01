6881233aea7d198e25739cef54beeeaba7f6695b
evaniewares <chisopsyelera@gmail.com>
2023-12-01 03:33:22 +0200

    Updated header file

--- binary_trees.h
+++ binary_trees.h
@@ -63,4 +63,22 @@ size_t binary_tree_size(const binary_tree_t *tree);
 /* 12-binary_tree_leaves.c */
 size_t binary_tree_leaves(const binary_tree_t *tree);
 
+/* 13-binary_tree_nodes.c */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/* 14-binary_tree_balance.c */
+int binary_tree_balance(const binary_tree_t *tree);
+
+/* 15-binary_tree_is_full.c */
+int binary_tree_is_full(const binary_tree_t *tree);
+
+/* 16-binary_tree_is_perfect.c */
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/* 17-binary_tree_sibling.c */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* 18-binary_tree_uncle.c */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
